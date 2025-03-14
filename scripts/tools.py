e3205467c2366016502b47ec80672bcee01efd72
bruno <brunosbedo@gmail.com>
2025-03-14 11:08:10 -0300

    add code to load tracking data

--- scripts/tools.py
+++ scripts/tools.py
@@ -36,3 +36,34 @@ def get_match_info(jogo_id):
     except Exception as e:
         print(f"Unexpected error while loading match info: {e}")
         return None
+
+
+
+def load_tracking(jogo_id):
+    try:
+        file = Path(f'{get_project_dir()}/data/2d/jogo{jogo_id}.2d')
+        # Check if the file exists before trying to open it
+        if not file.exists():
+            print(f"Error: File not found - {file}")
+            return None
+
+        # Load the Excel file
+        df = pd.read_csv(file, delim_whitespace=True, header=None)
+        print(f"Successfully loaded tracking data for match {jogo_id}.")
+        num_cols = df.shape[1]
+        new_column_names = [f"j{i//2 + 1}{'x' if i % 2 == 0 else 'y'}" for i in range(num_cols)]
+        df.columns = new_column_names
+        df = df.dropna(axis=1, how='all')
+        return df
+
+    except FileNotFoundError:
+        print(f"Error: File not found - {file}")
+        return None
+
+    except pd.errors.EmptyDataError:
+        print(f"Error: File is empty or corrupted - {file}")
+        return None
+
+    except Exception as e:
+        print(f"Unexpected error while loading match info: {e}")
+        return None
